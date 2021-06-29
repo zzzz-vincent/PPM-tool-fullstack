588ac7dd773db0b53d389dc0dd5ec5e272bb79d2
zzzz-vincent <wenzishen.vincent@hotmail.com>
2021-06-29 15:07:26 -0400

    update project find, delete and update functions

--- src/main/java/io/zwen/ppmtoolfullstack/services/ProjectService.java
+++ src/main/java/io/zwen/ppmtoolfullstack/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.zwen.ppmtoolfullstack.services;
 
 import io.zwen.ppmtoolfullstack.domain.Project;
+import io.zwen.ppmtoolfullstack.exception.ProjectIdException;
 import io.zwen.ppmtoolfullstack.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,35 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
+        try{
+            project.setProjectIdentifier((project.getProjectIdentifier().toUpperCase()));
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
 
-        return projectRepository.save(project);
+    }
+
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '"+projectId.toUpperCase()+"' does not exists");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProject(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectid){
+        Project project = projectRepository.findByProjectIdentifier(projectid.toUpperCase());
+        if(project == null){
+            throw new ProjectIdException("Cannot Project with ID '"+projectid+";. this project does not exist");
+        }
+        projectRepository.delete(project);
     }
 }
